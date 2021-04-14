1fb54af4e4cdbaad728f680fc847aa95667a5ac5
ShawnJHL <slee9118@gmail.com>
2021-04-14 15:03:12 -0500

    Update and rename web.go to web/basic.go

--- web/basic.go
+++ web/basic.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
+	"html/template"
+	"encoding/xml"
+	"io/ioutil"
 )
 
 func index_handler (w http.ResponseWriter, r *http.Request) {
@@ -18,10 +21,27 @@ func about_handler (w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf (w, "ABOUT PAGE!")
 }
 
+type NewsAggPage struct {
+  Title string
+  News map[string]NewsMap
+}
+
+/*
+basictemplating.html
+<h1>{{.Title}}</h1>
+<p>{{.News}}</p>
+*/
+func newsAggHandler (w http.ResponseWriter, r *http.Request) {
+  p := NewsAggPage {Title: "Amazing News Aggregator", News: "some news"}
+  t, _ := template.ParseFiles ("basictemplating.html")
+  t.Execute (w, p)
+}
+
 func main () {
   // TAKES URL PATH AND FIND CORRESPONDING FUNCTION. / IS THE HOMEPAGE. INDEX_HANDLER IS THE FUNCTION WE WANT TO RUN
   http.HandleFunc ("/", index_handler)
   http.HandleFunc ("/about/", about_handler)
+  http.HandleFunc ("/agg/", newsAggHandler)
   
   // CREATES A LOCAL SERVER AT PORT 8000
   http.ListenAndServe (":8000", nil)
